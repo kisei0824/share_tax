0826ad95e961667b06fc143dbe30bc898ca3a9ab
岩橋樹生 <iwahashikisei@iwahashimikionoMacBook-Pro.local>
2021-05-02 17:10:14 +0900

    トップ画の編集

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'mini_racer'
 gem 'bootstrap'
 gem 'bootstrap-sass'
+gem 'ransack'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,29 @@
 class PostsController < ApplicationController
   def index
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: true)
+    # @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save!
+    redirect_to posts_url
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:tax, :description)
   end
 end
